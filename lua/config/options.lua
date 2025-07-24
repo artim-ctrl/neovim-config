9277d8bda878157fc4536f3e0f52ba8e4891b687
Dmitrii Derenko <dmitrii.derenko@gmail.com>
2025-07-24 17:59:22 +0200

    Initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,18 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.keymap.set("n", "<Up>", "")
+vim.keymap.set("n", "<Down>", "")
+vim.keymap.set("n", "<Left>", "")
+vim.keymap.set("n", "<Right>", "")
+
+vim.keymap.set("i", "<Up>", "")
+vim.keymap.set("i", "<Down>", "")
+vim.keymap.set("i", "<Left>", "")
+vim.keymap.set("i", "<Right>", "")
+
+vim.keymap.set("v", "<Up>", "")
+vim.keymap.set("v", "<Down>", "")
+vim.keymap.set("v", "<Left>", "")
+vim.keymap.set("v", "<Right>", "")
