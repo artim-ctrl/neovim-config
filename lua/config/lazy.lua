6634c0cde8690c18a8f8d4f661df5571bb920454
Dmitrii Derenko <dmitrii.derenko@gmail.com>
2025-08-08 23:38:07 +0200

    still in progress

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,16 +19,16 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- add transparency to the theme
-    {
-      "folke/tokyonight.nvim",
-      opts = {
-        transparent = true,
-        styles = {
-          sidebars = "transparent",
-          floats = "transparent",
-        },
-      },
-    },
+    -- {
+    --   "folke/tokyonight.nvim",
+    --opts = {
+    --  transparent = true,
+    --  styles = {
+    --    sidebars = "transparent",
+    --    floats = "transparent",
+    --  },
+    --},
+    -- },
     -- import/override with your plugins
     { import = "plugins" },
     -- disable phpcs
@@ -54,9 +54,19 @@ require("lazy").setup({
               "$FILENAME",
             },
           },
+          ["golangci_lint"] = {
+            args = {
+              "fmt",
+              "--config=" .. vim.fn.getcwd() .. "/.golangci.yml",
+              "$FILENAME",
+            },
+            stdin = false,
+            cwd = vim.fn.getcwd,
+          },
         },
         formatters_by_ft = {
           php = { "php_cs_fixer" },
+          go = { "golangci_lint" },
         },
       },
     },
@@ -70,7 +80,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = { colorscheme = { "catppuccin" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
