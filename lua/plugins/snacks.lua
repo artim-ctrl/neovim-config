f155dbf56143a3b55eb2b5a56e823f7e8a538e21
Dmitrii Derenko <dmitrii.derenko@gmail.com>
2025-09-20 22:28:23 +0200

    Show ignored files by default

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,9 +3,11 @@ return {
   opts = {
     picker = {
       hidden = true,
+      ignored = true,
       sources = {
         files = {
           hidden = true,
+          ignored = true,
         },
       },
     },
